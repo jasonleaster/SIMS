d4dae98bfe46967196fc8abd4429a1b0b64f85b1
“jasonleaster” <jasonleaster@gmail.com>
2022-10-23 21:21:32 +0800

    删除无效代码

--- src/main/java/org/sims/web/config/DataConfig.java
+++ src/main/java/org/sims/web/config/DataConfig.java
@@ -2,19 +2,19 @@ package org.sims.web.config;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+//import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+//import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
 
 //@Component
-public class DataConfig {
-
-    public static void initDB(DataSource dataSource){
-        Resource resource = new ClassPathResource("SQL/schema.sql");
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-        databasePopulator.addScript(resource);
-        DatabasePopulatorUtils.execute(databasePopulator, dataSource);
-    }
-}
+//public class DataConfig {
+//
+//    public static void initDB(DataSource dataSource){
+//        Resource resource = new ClassPathResource("SQL/schema.sql");
+//        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+//        databasePopulator.addScript(resource);
+//        DatabasePopulatorUtils.execute(databasePopulator, dataSource);
+//    }
+//}
